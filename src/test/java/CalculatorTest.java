b3971c03650a60107f53a2bbd6c2324a742c00c3
gon2gon2 <ark5354@gmail.com>
2022-11-23 23:23:33 +0900

    feature: 기능 구현 완료

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,35 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    private static Calculator calculator = new Calculator();
+
+
+
+    @Test
+    void test_calc(){
+        assertThat(calculator.calc(1, "+", 1)).isEqualTo(2);
+        assertThat(calculator.calc(5, "-", 3)).isEqualTo(2);
+        assertThat(calculator.calc(1, "*", 2)).isEqualTo(2);
+        assertThat(calculator.calc(4, "/", 2)).isEqualTo(2);
+    }
+
+    @Test
+    void test_accumulate(){
+        String[] values = {"1", "+", "1"};
+        assertThat(calculator.accumulate(values)).isEqualTo(2);
+    }
+
+    //입력값의 순서에 따라 계산함
+    @Test
+    void test_sequential_calculation(){
+        String[] values = {"1", "+", "2", "*", "2"};
+        assertThat(calculator.accumulate(values)).isEqualTo(6);
+    }
+
+
+}
